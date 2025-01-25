59c38cde7269379dd175ed5b87b0215de100ae0c
seoulorigin <56393776+seoulorigin@users.noreply.github.com>
2025-01-25 14:20:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 여기서 사용자 입장에서 꼭 입력해야 하는 정보를 의사결정해서 개발에 반영할 필요가 있습니다.
 const siteConfig = {
   username: "seoulorigin", // GitHub 사용자 이름
-  repositoryName: "", // GitHub 저장소 이름
+  repositoryName: "techblog", // GitHub 저장소 이름
   mainColor: "#3498db", // 사이트의 주 색상
   textColor: "#333333", // 기본 텍스트 색상
   blogTitle: "seoulorigin_techblog", // 블로그 제목
